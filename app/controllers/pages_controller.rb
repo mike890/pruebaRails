5c6ba20125e4bec7ddd54f8053ee2123202a8882
Mike <mapv890@gmail.com>
2017-03-27 21:53:00 -0600

    otro mensaje mas

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,5 @@
 class PagesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def batman
   end
 
@@ -6,5 +7,17 @@ class PagesController < ApplicationController
   end
 
   def batmanvssuperman
+    @count1 = Votosuperman.count
+    @count2 = Votsbatman.count
+  end
+
+  def voto_superman
+    Votosuperman.new(nombre: params[:nombre], email: params[:email]).save
+    redirect_to pages_batmanvssuperman_path, notice: 'el voto para superman se hizo correctamente'
+  end
+
+  def voto_batman
+    Votsbatman.new(nombre: params[:nombre], email: params[:email]).save
+    redirect_to pages_batmanvssuperman_path, notice: 'el voto para batman se hizo correctamente'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
 
   get 'pages/batmanvssuperman'
 
+  post 'pages/voto_superman'
+
+  post 'pages/voto_batman'
+
   root 'pages#batmanvssuperman'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
