3ee1ad59d5e2540fd00b03441a8ad3fe30e1da14
Mike <mapv890@gmail.com>
2017-03-27 20:59:22 -0600

    tercer commit de la prueba

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,7 @@ Rails.application.routes.draw do
 
   get 'pages/batmanvssuperman'
 
+  root 'pages#batmanvssuperman'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
